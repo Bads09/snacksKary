03ff9f46898c4f9926b9d18d7ca0d3aa3defddba
bads09 <bads09@hotmail.com>
2024-04-17 20:45:16 -0600

    precios a alimentos

--- src/pages/Alimentos.js
+++ src/pages/Alimentos.js
@@ -7,20 +7,43 @@ import Pulpos from "./Pulpos.jpg";
 import Tacos from "./Tacos.jpg";
 import Tostitos from "./Tostitos.jpg";
 import Footer from "./Footer";
+import { GrBottomCorner } from "react-icons/gr";
 
 const alimentos = () =>{
     return (
 <div className="fondo">
     <h1>Contamos con una gran variedad de alimentos</h1>
     <h2>Revisa nuestro menú y ordena tus alimentos</h2>
-    <img className="alimento" src= {Alas}/>
-    <img className="alimento" src= {Dedos}/>
-    <img className="alimento" src= {Aros}/>
-    <img className="alimento" src= {Nachos}/>
-    <img className="alimento" src= {Pulpos}/>
-    <img className="alimento" src= {Tacos}/>
-    <img className="alimento" src= {Tostitos}/>
-    <Footer/>
+    <div class="alimento">
+  <img className="alimento" src= {Alas}/>
+  <div class="desc">orden de 10 alitas con vegetales incluidos $120</div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Aros}/>
+  <div class="desc">orden de 10 aros de cebolla $80</div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Dedos}/>
+  <div class="desc">orden de 6 aros de cebolla $70</div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Nachos}/>
+  <div class="desc">Nachos ch: $55 G: $70</div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Pulpos}/>
+  <div class="desc">Salchipulpos Ch: $40 G: $55 EG: $70</div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Tacos}/>
+  <div class="desc">Salchitacos Ch: $40 G: $55 EG: $70 </div>
+</div>
+<div class="alimento">
+  <img className="alimento" src= {Tostitos}/>
+  <div class="desc">Tostilocos $55</div>
+</div>
+
+
 </div>
 
     );
--- src/pages/Pedido.js
+++ src/pages/Pedido.js
@@ -1,9 +1,11 @@
+import Footer from "./Footer";
+
 const pedido = () =>{
-    return 
-    
-    
-
-
+    return  <div className="fondo">
+    <h1>Tu pedido se ha registrado </h1>
+    <h2>Rregistra tus datos, a dónde será enviado el pedido</h2>
+    <Footer></Footer>
+</div>
 }
 
 export default pedido;
